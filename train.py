69d859588aaea0275f6884493548eba58d1546d9
robietanam <robietanam@gmail.com>
2025-04-20 14:05:00 +0700

    forgor

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("ultralytics/cfg/models/11/modified/yolo11-Starnet-C2f-Star-LADH.yaml")  
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-Starnet-C2f-Star-LSCD.yaml")  
 
 # Train the model
 results = model.info(verbose=True, detailed=True)
