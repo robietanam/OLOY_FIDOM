6671b7a7d9970fe54b10594b686d3d6aa063f977
robietanam <robietanam@gmail.com>
2025-04-22 13:49:06 +0700

    lscd ablation

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml")  
+model = YOLO("ultralytics/cfg/models/11/yolo11.yaml")  
 # Train the model
 results = model.info(verbose=True, detailed=False)
+
+
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-C3k2-Star-LSCD.yaml")  
+# Train the model
+results = model.info(verbose=True, detailed=False)
+
