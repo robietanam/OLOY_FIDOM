b36b4c1037281d70598363d687b0000bc6b708b9
robietanam <robietanam@gmail.com>
2025-04-23 11:34:45 +0700

    try this

--- train.py
+++ train.py
@@ -6,7 +6,48 @@ results = model.info(verbose=True, detailed=False)
 
 
 
-model = YOLO("ultralytics/cfg/models/11/modified/yolo11-C3k2-Star-LSCD.yaml")  
-# Train the model
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-Starnet.yaml")  
 results = model.info(verbose=True, detailed=False)
 
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-C3k2-Star.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-LSCD.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-Starnet-C3k2-Star-LSCD.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-C3k2-Star-LSCD.yaml") 
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-C3k2-Star-LADH.yaml") 
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-starnet.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-star.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-LSCD.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-starnet-C2f-star-LSCD.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-star-LSCD.yaml")  
+results = model.info(verbose=True, detailed=False)
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-star-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
