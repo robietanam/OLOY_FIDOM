83051bdeef40ea4a28d3362f411b17242cb7db60
robietanam <robietanam@gmail.com>
2025-05-03 20:10:17 +0700

    bifpn ladh

--- train.py
+++ train.py
@@ -9,6 +9,12 @@ results = model.fuse()
 
 
 
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-Star-LADH.yaml")  
+
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+
 model = YOLO("ultralytics/cfg/models/11/yolo11.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
