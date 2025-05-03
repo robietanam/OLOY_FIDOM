3fbabc8bcb2b82b81bef4ca0966f564ee4d7c3e0
robietanam <robietanam@gmail.com>
2025-05-03 20:26:42 +0700

    also this

--- train.py
+++ train.py
@@ -10,10 +10,18 @@ results = model.fuse()
 
 
 model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-Star-LADH.yaml")  
-
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
 
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-C2f-Star-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
 
 model = YOLO("ultralytics/cfg/models/11/yolo11.yaml")  
 results = model.info(verbose=True, detailed=False)
