3b51ef3de3dfb8022274a00598bdecb507129c2e
robietanam <robietanam@gmail.com>
2025-05-05 11:19:55 +0700

    yolo11 bifpn

--- train.py
+++ train.py
@@ -2,27 +2,48 @@ from ultralytics import YOLO
 
 
 
-model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml")  
-
+model = YOLO("ultralytics/cfg/models/11/yolo11.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
 
-
+model = YOLO("ultralytics/cfg/models/v8/yolov8.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
 
 model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-C2f-Star-LADH.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
 
-
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
 model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-C2f-Star-LADH.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
 
 
-model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-LADH.yaml")  
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-bifpn-BC2f-Star-LADH.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
 
-model = YOLO("ultralytics/cfg/models/11/yolo11.yaml")  
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-starnet-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-starnet-bifpn.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+model = YOLO("ultralytics/cfg/models/v8/modified/yolov8-starnet-bifpn-LADH.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-bifpn.yaml")  
+results = model.info(verbose=True, detailed=False)
+results = model.fuse()
+
+
+model = YOLO("ultralytics/cfg/models/11/modified/yolo11-bifpn-LADH.yaml")  
 results = model.info(verbose=True, detailed=False)
 results = model.fuse()
--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -17,7 +17,7 @@ from .conv import Conv, DWConv, autopad
 from .transformer import MLP, DeformableTransformerDecoder, DeformableTransformerDecoderLayer
 from .utils import bias_init_with_prob, linear_init
 
-__all__ = "Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder", "v10Detect", "YOLOEDetect", "YOLOESegment", "Detect_LADH" , "Detect_LSCD", "Detect_TADDH"
+__all__ = "Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder", "v10Detect", "YOLOEDetect", "YOLOESegment", "Detect_LADH" , "DSConv" , "Detect_LSCD", "Detect_TADDH"
 
 
 class Detect(nn.Module):
